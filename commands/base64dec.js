750144577a746621c529279ea771dbb0494504af
RMED24 <81475204+RMED24@users.noreply.github.com>
2021-06-25 20:24:29 +0100

    Update base64dec.js

--- commands/base64dec.js
+++ commands/base64dec.js
@@ -4,8 +4,8 @@ module.exports = {
     
     execute(message, args){
   
-        let data = message.content.toString;
-        let buff = new Buffer(data);
+        let data = 'SGVsbG8gV29ybGQ=';
+        let buff = new Buffer.from(data);
         let base64data = Buffer.from(data, 'base64').toString('ascii');
         
         message.channel.send('"' + data + '" converted to ASCII (standard text) is "' + base64data + '"');
